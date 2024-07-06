08661e4378e933209811da178d9822f24e545f97
Danekim-67 <youngmoon06070@gmail.com>
2024-07-06 16:48:59 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.py
@@ -0,0 +1,9 @@
+a=int(input())
+for i in range(2*a):
+    print("@"*a+" "*3*a+"@"*a)
+for i in range(a):
+    print("@"*5*a)
+for i in range(a):
+    print("@"*a+" "*3*a+"@"*a)
+for i in range(a):
+    print("@"*5*a)
