72a74e3a7c5dbfc68ef8de2b212b5baee2bf984d
Danekim-67 <youngmoon06070@gmail.com>
2024-07-06 16:52:36 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅈ, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23809. 골뱅이 찍기 － 돌아간 ㅈ/골뱅이 찍기 － 돌아간 ㅈ.py
@@ -0,0 +1,11 @@
+a=int(input())
+for i in range(a):
+    print("@"*a+" "*3*a+"@"*a)
+for i in range(a):
+    print("@"*a+" "*2*a+"@"*a)
+for i in range(a):
+    print("@"*a*3)
+for i in range(a):
+    print("@"*a+" "*2*a+"@"*a)
+for i in range(a):
+    print("@"*a+" "*3*a+"@"*a)
